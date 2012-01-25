aa4b12481aa71e1a4c8da62dfecf87bbf20e01c2
Eric E Monson <emonson@cs.duke.edu>
2012-01-25 15:51:44 -0500

    Moved US to another dir & improved google scholar

--- google_scholar_test.py
+++ google_scholar_test.py
@@ -2,6 +2,7 @@ import urllib
 import httplib
 from BeautifulSoup import BeautifulSoup
 import re
+import time
 
 case_addr = re.compile('(/scholar_case\?case=[0-9]+)&')
 
@@ -52,33 +53,59 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-u
 # 
 # print url
 
-url = '/scholar?hl=en&num=100&q=%2B%2217+USC%22&btnG=Search&as_sdt=4%2C56%2C57&as_ylo=1994&as_yhi=1994&as_vis=0'
+url = '/scholar?start=700&q=goods+appropriation&hl=en&num=100&as_sdt=3,34&as_ylo=1950&as_yhi=1980'
+# url = '/scholar?hl=en&num=100&q=%2B%2217+USC%22&btnG=Search&as_sdt=4%2C56%2C57&as_ylo=1994&as_yhi=1994&as_vis=0'
 # url = '/scholar?hl=en&num=100&q=%2217+USC%22&btnG=Search&as_sdt=4%2C56%2C57&as_ylo=2006&as_yhi=2006&as_vis=0'
 
 conn = httplib.HTTPConnection(host)
-conn.request("GET", url, {}, headers)
 
-resp = conn.getresponse()
-
-if resp.status == 200:
-	html = resp.read()
+while url != None:
+	print 'delaying query'
+	time.sleep(1.0)
+	conn.request("GET", url, None, headers)
 	
-	f = open('g1994.html', 'w')
-	f.write(html)
-	f.close()
+	resp = conn.getresponse()
 	
-	soup = BeautifulSoup(html)
-	# Looks like <div class=gs_r> </div> encloses each full result record
-	# and <div class=gs_rt> </div> encloses each result title & link
-	
-	# Here taking links with a case href, plus real links (not cited_by) have "onmousedown" field
-	cases_list = soup.findAll(attrs={'href':re.compile("^/scholar_case"), 'onmousedown':True})
-	
-	# TODO: Should really also search within returned summary for 17 USC string...
-	
-	
-	for ii, case in enumerate(cases_list):
-		addr = case_addr.findall(case['href'])
-		print ii, addr[0]
+	if resp.status == 200:
+		html = resp.read()
 		
+		f = open('g1994.html', 'w')
+		f.write(html)
+		f.close()
+		
+		soup = BeautifulSoup(html)
+		# Looks like <div class=gs_r> </div> encloses each full result record
+		# and <div class=gs_rt> </div> encloses each result title & link
+		
+		# Here taking links with a case href, plus real links (not cited_by) have "onmousedown" field
+		cases_list = soup.findAll(attrs={'href':re.compile("^/scholar_case"), 'onmousedown':True})
+		
+		# TODO: Should really also search within returned summary for 17 USC string...
+		
+		
+		for ii, case in enumerate(cases_list):
+			addr = case_addr.findall(case['href'])
+			print ii, addr[0]
+			# Get first case on page for now
+			if ii == 0:
+				print 'delaying page request'
+				time.sleep(1.0)
+				conn.request("GET", addr[0], None, headers)
+				resp = conn.getresponse()
+				if resp.status == 200:
+					case_html = resp.read()
+					print case_html
+		
+		# Getting to following pages
+		# Div containing bottom navigation table of links
+		nav_div = soup.find('div', {"class" : "n"})
+		nav_links = nav_div.findAll('a')
+		for link in nav_links:
+			if link.find('span',{'class':'SPRITE_nav_next'}) != None:
+				url = link['href']
+			else:
+				url = None
+		# Span containing prev/next words in prev/next links
+		# sb = soup.findAll('span', {'class':'b'})
+
 
