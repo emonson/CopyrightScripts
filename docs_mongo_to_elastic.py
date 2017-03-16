862e5d2eb0af848647bf1cb2d95519071a00adc0
Eric E Monson <emonson@cs.duke.edu>
2017-03-16 10:51:08 -0400

    shifted docs mongo to elastic over to AWS ES specs

--- docs_mongo_to_elastic.py
+++ docs_mongo_to_elastic.py
@@ -1,8 +1,7 @@
 import sys
-from pymongo import Connection
+from pymongo import MongoClient
 from datetime import datetime
-from elasticsearch import Elasticsearch
-from elasticsearch import helpers
+from elasticsearch import Elasticsearch, RequestsHttpConnection, helpers
 import pprint
 import time
 
@@ -15,36 +14,40 @@ DRY_RUN = False
 # Get data directly from MongoDB
 # Make a connection to Mongo.
 try:
-    db_conn = Connection()
+    client = MongoClient()
 except ConnectionFailure:
     print "couldn't connect: be sure that Mongo is running on localhost:27017"
     sys.exit(1)
 
-db = db_conn[mongo_db_name]
+db = client[mongo_db_name]
 
 # Now also make a connection to Elasticsearch
-es = Elasticsearch()
+host = 'search-dvstest-ssbfvmt2tjfvm7jj6qh2jtmuvi.us-west-2.es.amazonaws.com'
+es = Elasticsearch( hosts=[{'host':host, 'port':443}], 
+    use_ssl=True, 
+    verify_certs=True, 
+    connection_class=RequestsHttpConnection
+)
 
 # DANGER -- Delete index!!
-if not DRY_RUN:
+if not DRY_RUN and es.indices.exists(es_index_name):
     es.indices.delete(es_index_name)
 
 case_mapping = { "properties": {
-    "content": { "analyzer": "english", "type": "string" },
-    "court": { "index": "not_analyzed", "type": "string" },
-    "court_level_name": { "index": "not_analyzed", "type": "string" },
+    "content": { "analyzer": "english", "type": "text" },
+    "court": { "type": "keyword" },
+    "court_level_name": { "type": "keyword" },
     "court_level": { "type": "integer" },
-    "subject_tags": { "index": "not_analyzed", "type": "string" },
-    "tags": { "index": "not_analyzed", "type": "string" },
+    "subject_tags": { "type": "keyword" },
+    "tags": { "type": "keyword" },
     "year": { "format": "year", "type": "date" }
   }
 }
 
 # Create the index
-if not es.indices.exists( index = es_index_name ):
+if (not DRY_RUN) and (not es.indices.exists( index = es_index_name )):
     es.indices.create( index = es_index_name, body={ "number_of_shards": 1 } )
     es.indices.put_mapping(index=es_index_name, doc_type=es_doc_type, body=case_mapping)
-
 
 # pp = pprint.PrettyPrinter(indent=2)
 # pp.pprint( es.indices.get_settings( index = es_index_name ) )
@@ -52,21 +55,15 @@ if not es.indices.exists( index = es_index_name ):
 # Direct from MongoDB method
 ii = 0
 
-# Storing iterator of actions to feed to bulk api
-# NOTE: Potential memory problems since gatering them all in memory between feeding to ES!
-actions = []
 start_time = time.time()
 
 for doc in db.docs.find({},{'solr_term_list':False, 'solr_term_freqs':False }):
-    if ii % 1000 == 0:
-        # Actually feed docs to elasticsearch bulk api for indexing
-        res = helpers.bulk(es, actions)
-        print ii, res, time.time() - start_time
-        actions = []
+    if ii % 100 == 0:
+        print ii, time.time() - start_time
     
     # Replace a couple pieces that ES can't serialize from the mongo object
-    id_str = str(doc['_id'])
-    doc['_id'] = id_str
+    id_str = doc.pop('_id')
+    # doc['_id'] = id_str
     ref_coll = doc['file_ref'].collection
     ref_id_str = str(doc['file_ref'].id)
     doc['file_ref'] = { 'collection':ref_coll, 'id':ref_id_str }
@@ -102,13 +99,12 @@ for doc in db.docs.find({},{'solr_term_list':False, 'solr_term_freqs':False }):
         # NOTE: If no date (only 16 of them), use the year, and put it in the middle
         doc['date_min'] = datetime.strptime(str(doc['year'])+'-06-01', '%Y-%m-%d')
     
-    # Add doc to actions list
-    doc.update({'_index':es_index_name, '_type':es_doc_type, '_op_type':'create' })
-    actions.append(doc)
+    # Having trouble with AWS ES limitations of not specifying index and doc type 
+    # in bulk call, and python Elasticsearch bulk helper needing it, so trying individual instead
+    if not DRY_RUN:
+        res = es.index(index=es_index_name, doc_type=es_doc_type, id=id_str, op_type='create', body=doc)
     
     ii += 1
 
-# Actually feed docs to elasticsearch bulk api for indexing
-res = helpers.bulk(es, actions)
-print res, time.time() - start_time
+print 'Done:', time.time() - start_time
 
